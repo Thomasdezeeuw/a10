9a5be08134676832862545c6d8f3ac4d68342c98
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-08-14 11:29:02 +0200

    Add example to Extract trait

--- src/extract.rs
+++ src/extract.rs
@@ -13,8 +13,8 @@
 /// To ensure the input stays alive A10 needs ownership of the input arguments
 /// and leaks the inputs when a [`Future`] operation is dropped before
 /// completion. However to give the `Future`s a nice API we don't return the
-/// input arguments and try to match the API that don't take ownership of
-/// argument, e.g [`fs::File::open`] just returns a [`File`] not the path
+/// input arguments and try to match the APIs that don't take ownership of
+/// arguments, e.g [`fs::File::open`] just returns a [`File`] not the path
 /// argument.
 ///
 /// In some cases though we would like to get back the input arguments from the
@@ -27,6 +27,42 @@
 /// [`fs::File::open`]: crate::fs::File::open
 /// [`File`]: crate::fs::File
 /// [supported operations below]: #implementors
+///
+/// # Examples
+///
+/// Extracting the argument from [`fs::File::open`] and [`fs::File::write`]
+/// operations.
+///
+/// ```
+/// use std::io;
+/// use std::path::PathBuf;
+///
+/// use a10::fs::File;
+/// use a10::{SubmissionQueue, Extract};
+///
+/// async fn write_all_to_file(sq: SubmissionQueue, path: PathBuf, buf: Vec<u8>) -> io::Result<(PathBuf, Vec<u8>)> {
+///     // This `Future` returns just the opened file.
+///     let open_file_future = File::open(sq, path)?;
+///     // Calling `extract` and awaiting that will return both the file and the
+///     // path.
+///     let (file, path) = open_file_future.extract().await?;
+///
+///     // This just returned the number of bytes written.
+///     let write_future = file.write(buf)?;
+///     // When extracing it also returns the buffer.
+///     let (buf, n) = write_future.extract().await?;
+///
+///     if n != buf.len() {
+///         // NOTE: `WriteZero` is not entirely accurate, but just for the sake
+///         // of the example.
+///         return Err(io::Error::new(io::ErrorKind::WriteZero, "didn't write entire buffer"));
+///     }
+///
+///     Ok((path, buf))
+/// }
+/// ```
+///
+/// [`fs::File::write`]: crate::fs::File::write
 pub trait Extract {
     /// Extract input arguments from the future.
     fn extract(self) -> Extractor<Self>
