64dff160b5fdbb207b5facb063ab952544becdea
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2022-12-24 17:06:33 +0000

    Remove MaybeUninitSlice
    Currently unused.

--- src/io.rs
+++ src/io.rs
@@ -2,12 +2,10 @@
 
 use std::cell::UnsafeCell;
 use std::future::Future;
-use std::marker::PhantomData;
-use std::mem::{ManuallyDrop, MaybeUninit};
-use std::ops::{Deref, DerefMut};
+use std::mem::ManuallyDrop;
 use std::pin::Pin;
 use std::task::{self, Poll};
-use std::{fmt, io, ptr, result, slice};
+use std::{io, ptr, result};
 
 use crate::op::{op_future, NO_OFFSET};
 use crate::{libc, AsyncFd, OpIndex, QueueFull, SubmissionQueue};
@@ -199,48 +197,6 @@ impl Future for Close {
 
     fn poll(self: Pin<&mut Self>, ctx: &mut task::Context<'_>) -> Poll<Self::Output> {
         self.sq.poll_op(ctx, self.op_index).map_ok(|_| ())
-    }
-}
-
-/// A version of [`IoSliceMut`] that allows the buffer to be uninitialised.
-///
-/// [`IoSliceMut`]: std::io::IoSliceMut
-#[repr(transparent)]
-pub struct MaybeUninitSlice<'a> {
-    vec: libc::iovec,
-    _lifetime: PhantomData<&'a mut [MaybeUninit<u8>]>,
-}
-
-impl<'a> MaybeUninitSlice<'a> {
-    /// Creates a new `MaybeUninitSlice` wrapping a byte slice.
-    pub const fn new(buf: &'a mut [MaybeUninit<u8>]) -> MaybeUninitSlice<'a> {
-        MaybeUninitSlice {
-            vec: libc::iovec {
-                iov_base: buf.as_mut_ptr().cast(),
-                iov_len: buf.len(),
-            },
-            _lifetime: PhantomData,
-        }
-    }
-}
-
-impl<'a> Deref for MaybeUninitSlice<'a> {
-    type Target = [MaybeUninit<u8>];
-
-    fn deref(&self) -> &[MaybeUninit<u8>] {
-        unsafe { slice::from_raw_parts(self.vec.iov_base.cast(), self.vec.iov_len) }
-    }
-}
-
-impl<'a> DerefMut for MaybeUninitSlice<'a> {
-    fn deref_mut(&mut self) -> &mut [MaybeUninit<u8>] {
-        unsafe { slice::from_raw_parts_mut(self.vec.iov_base.cast(), self.vec.iov_len) }
-    }
-}
-
-impl<'a> fmt::Debug for MaybeUninitSlice<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.deref().fmt(f)
     }
 }
 
