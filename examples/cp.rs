2e4e14561919f769e3aabe26f2fa7b3ebbde7aa4
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2023-02-19 00:42:11 +0000

    Add cp example
    Copy files.

---
+++ examples/cp.rs
@@ -0,0 +1,77 @@
+//! cp - copy files.
+//!
+//! Only a single file copy is supported.
+
+use std::env::args;
+use std::io;
+
+use a10::fs::OpenOptions;
+use a10::{Extract, SubmissionQueue};
+
+mod runtime;
+
+fn main() -> io::Result<()> {
+    // Create a new I/O uring.
+    let mut ring = a10::Ring::new(1)?;
+    // Get our copy of the submission queue.
+    let sq = ring.submission_queue().clone();
+
+    // Collect the files we want to concatenate.
+    let mut args = args().skip(1);
+    let source = match args.next() {
+        Some(source) => source,
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "missing source",
+            ))
+        }
+    };
+    let destination = match args.next() {
+        Some(destination) => destination,
+        None => {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "missing destination",
+            ))
+        }
+    };
+
+    // Run our cat program.
+    runtime::block_on(&mut ring, cp(sq, source, destination))
+}
+
+async fn cp(sq: SubmissionQueue, source: String, destination: String) -> io::Result<()> {
+    let input = OpenOptions::new()
+        .read()
+        .open(sq.clone(), source.into())
+        .await?;
+    let output = OpenOptions::new()
+        .write()
+        .create_new()
+        .open(sq.clone(), destination.into())
+        .await?;
+
+    // Read and write 8 pages at a time.
+    let mut buf = Vec::with_capacity(8 * 4096);
+    let mut n;
+    loop {
+        buf.clear();
+        buf = input.read(buf).await?;
+        if buf.is_empty() {
+            // Read the entire file.
+            return Ok(());
+        }
+
+        loop {
+            (buf, n) = output.write(buf).extract().await?;
+            if n == buf.len() {
+                // Written all the bytes, try reading again.
+                break;
+            } else {
+                // Remove the bytes we've already written and try again.
+                buf.drain(..n);
+            }
+        }
+    }
+}
