0b508022da6bd37ce31142cf9a602c44e53d58a0
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-01-13 23:49:43 +0100

    Add IORING_OP_SENDMSG(_ZC) to fmt::Debug

--- src/op.rs
+++ src/op.rs
@@ -739,6 +739,8 @@ impl fmt::Debug for Submission {
             }
             libc::IORING_OP_SEND => net_op(&mut f, &self.inner, "IORING_OP_SEND"),
             libc::IORING_OP_SEND_ZC => net_op(&mut f, &self.inner, "IORING_OP_SEND_ZC"),
+            libc::IORING_OP_SENDMSG => net_op(&mut f, &self.inner, "IORING_OP_SENDMSG"),
+            libc::IORING_OP_SENDMSG_ZC => net_op(&mut f, &self.inner, "IORING_OP_SENDMSG_ZC"),
             libc::IORING_OP_RECV => net_op(&mut f, &self.inner, "IORING_OP_RECV"),
             libc::IORING_OP_SHUTDOWN => {
                 f.field("opcode", &"IORING_OP_SHUTDOWN")
