c6d1fbb672967403aeb8dc53b523dd2f1866a29a
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-02-18 14:49:03 +0100

    Add Config::with_direct_descriptors
    Enables the setup required for direct descriptors.

--- src/config.rs
+++ src/config.rs
@@ -21,6 +21,7 @@ pub struct Config<'r> {
     kernel_thread: bool,
     cpu_affinity: Option<u32>,
     idle_timeout: Option<u32>,
+    direct_descriptors: Option<u32>,
     attach: Option<&'r SubmissionQueue>,
 }
 
@@ -60,6 +61,7 @@ impl<'r> Config<'r> {
             kernel_thread: true,
             cpu_affinity: None,
             idle_timeout: None,
+            direct_descriptors: None,
             attach: None,
         }
     }
@@ -135,6 +137,21 @@ impl<'r> Config<'r> {
             u32::MAX
         };
         self.idle_timeout = Some(ms);
+        self
+    }
+
+    /// Enable direct descriptors.
+    ///
+    /// This registers a sparse array of `size` direct descriptor slots enabling
+    /// direct descriptors to be used. If this is not used attempt to create a
+    /// direct descriptor will result in `ENXIO`.
+    ///
+    /// By default direct descriptors are not enabled.
+    #[doc(alias = "IORING_REGISTER_FILES")]
+    #[doc(alias = "IORING_REGISTER_FILES2")]
+    #[doc(alias = "IORING_RSRC_REGISTER_SPARSE")]
+    pub const fn with_direct_descriptors(mut self, size: u32) -> Self {
+        self.direct_descriptors = Some(size);
         self
     }
 
@@ -224,6 +241,22 @@ impl<'r> Config<'r> {
 
         let cq = mmap_completion_queue(fd.as_fd(), &parameters)?;
         let sq = mmap_submission_queue(fd, &parameters)?;
+
+        if let Some(size) = self.direct_descriptors {
+            let register = libc::io_uring_rsrc_register {
+                flags: libc::IORING_RSRC_REGISTER_SPARSE,
+                nr: size,
+                resv2: 0,
+                data: 0,
+                tags: 0,
+            };
+            sq.register(
+                libc::IORING_REGISTER_FILES2,
+                (&register as *const libc::io_uring_rsrc_register).cast(),
+                size_of::<libc::io_uring_rsrc_register>() as _,
+            )?;
+        }
+
         Ok(Ring { cq, sq })
     }
 }
