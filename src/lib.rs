1e8c40e62906b50a5422022f1c661a2791b29605
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-09-29 19:21:58 +0200

    Add kqueue module

--- src/lib.rs
+++ src/lib.rs
@@ -145,6 +145,19 @@ mod drop_waker;
 #[cfg(target_os = "linux")]
 mod io_uring;
 
+#[cfg(any(
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "ios",
+    target_os = "macos",
+    target_os = "netbsd",
+    target_os = "openbsd",
+    target_os = "tvos",
+    target_os = "visionos",
+    target_os = "watchos",
+))]
+mod kqueue;
+
 #[rustfmt::skip] // This must come before the other modules for the documentation.
 pub mod fd;
 pub mod io;
