07b81cc07477b64708fea93520529607d8f0486b
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-09-29 19:00:48 +0200

    Add Direct descriptors back
    io_uring only.

--- src/fd.rs
+++ src/fd.rs
@@ -9,6 +9,9 @@ use std::{fmt, io};
 
 use crate::SubmissionQueue;
 
+#[cfg(target_os = "linux")]
+pub use crate::io_uring::fd::Direct;
+
 /// An open file descriptor.
 ///
 /// All functions on `AsyncFd` are asynchronous and return a [`Future`].
@@ -204,50 +207,6 @@ impl private::Descriptor for File {
     /* TODO(port).
     fn sync_close(fd: RawFd) -> io::Result<()> {
         syscall!(close(fd))?;
-        Ok(())
-    }
-    */
-}
-
-// TODO(port): move io_uring only code.
-
-/// Direct descriptors are io_uring private file descriptors.
-///
-/// They avoid some of the overhead associated with thread shared file tables
-/// and can be used in any io_uring request that takes a file descriptor.
-/// However they cannot be used outside of io_uring.
-#[derive(Copy, Clone, Debug)]
-pub enum Direct {}
-
-impl Descriptor for Direct {}
-
-impl private::Descriptor for Direct {
-    /* TODO(port).
-    fn use_flags(submission: &mut Submission) {
-        submission.use_direct_fd();
-    }
-
-    fn create_flags(submission: &mut Submission) {
-        submission.create_direct_fd();
-    }
-
-    fn cloexec_flag() -> libc::c_int {
-        0 // Direct descriptor always have (the equivalant of) `O_CLOEXEC` set.
-    }
-
-    fn cancel_flag() -> u32 {
-        libc::IORING_ASYNC_CANCEL_FD_FIXED
-    }
-    */
-
-    fn fmt_dbg() -> &'static str {
-        "direct descriptor"
-    }
-
-    /* TODO(port).
-    fn sync_close(fd: RawFd) -> io::Result<()> {
-        // TODO: don't leak the the fd.
-        log::warn!("leaking direct descriptor {fd}");
         Ok(())
     }
     */
