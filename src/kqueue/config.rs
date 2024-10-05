6ca3e0f00fb4f36b255e296135cf2bc028934b90
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-10-05 22:37:07 +0200

    Implement waking

--- src/kqueue/config.rs
+++ src/kqueue/config.rs
@@ -1,11 +1,11 @@
 //! kqueue configuration.
 
-use std::io;
 use std::marker::PhantomData;
 use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
 use std::sync::Mutex;
+use std::{io, mem, ptr};
 
-use crate::{sys, syscall, Ring};
+use crate::{sys, syscall, Ring, WAKE_ID};
 
 #[derive(Debug, Clone)]
 #[must_use = "no ring is created until `a10::Config::build` is called"]
@@ -28,7 +28,23 @@ impl<'r> Config<'r> {
     pub fn build(self) -> io::Result<Ring> {
         // SAFETY: `kqueue(2)` ensures the fd is valid.
         let kq = unsafe { OwnedFd::from_raw_fd(syscall!(kqueue())?) };
-        syscall!(fcntl(kq.as_raw_fd(), libc::F_SETFD, libc::FD_CLOEXEC))?;
+        let kq_fd = kq.as_raw_fd();
+        syscall!(fcntl(kq_fd, libc::F_SETFD, libc::FD_CLOEXEC))?;
+
+        // Set up waking.
+        let mut kevent = libc::kevent {
+            ident: 0,
+            filter: libc::EVFILT_USER,
+            flags: libc::EV_ADD | libc::EV_CLEAR | libc::EV_RECEIPT,
+            udata: WAKE_ID as _,
+            // SAFETY: all zeros is valid for `libc::kevent`.
+            ..unsafe { mem::zeroed() }
+        };
+        syscall!(kevent(kq_fd, &kevent, 1, &mut kevent, 1, ptr::null()))?;
+        if (kevent.flags & libc::EV_ERROR) != 0 && kevent.data != 0 {
+            return Err(io::Error::from_raw_os_error(kevent.data as i32));
+        }
+
         let change_list = Mutex::new(Vec::new());
         let shared = sys::Shared { kq, change_list };
         let poll = sys::Completions::new(self.events_capacity as usize);
