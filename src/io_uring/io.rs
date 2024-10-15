57618ea764be0df0c796fdf44179b2cc5708f530
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-10-15 19:55:35 +0200

    Implement Read operation for io_uring

---
+++ src/io_uring/io.rs
@@ -0,0 +1,67 @@
+use std::io;
+use std::marker::PhantomData;
+use std::task::Poll;
+
+use crate::fd::{AsyncFd, Descriptor};
+use crate::io::BufMut;
+use crate::op::{OpResult, Operation};
+use crate::sys::{self, libc};
+use crate::syscall;
+
+// Re-export so we don't have to worry about import `std::io` and `crate::io`.
+pub(crate) use std::io::*;
+
+pub(crate) struct Read<B>(PhantomData<*const B>);
+
+// TODO(port): create and use short version.
+impl<B: BufMut> crate::op::Op for Read<B> {
+    type Output = B;
+    type Resources = B;
+    type Args = u64; // Offset.
+    type Submission = sys::sq::Submission;
+    type CompletionState = sys::cq::CompletionResult;
+    /// Buffer index and operation output.
+    type OperationOutput = (u16, u32);
+
+    /// Fill a submission for the operation.
+    fn fill_submission<D: Descriptor>(
+        fd: &AsyncFd<D>,
+        buf: &mut Self::Resources,
+        offset: &mut Self::Args,
+        submission: &mut Self::Submission,
+    ) {
+        let (ptr, len) = unsafe { buf.parts_mut() };
+        submission.0.opcode = libc::IORING_OP_READ as u8;
+        submission.0.fd = fd.fd();
+        submission.0.__bindgen_anon_1 = libc::io_uring_sqe__bindgen_ty_1 { off: *offset };
+        submission.0.__bindgen_anon_2 = libc::io_uring_sqe__bindgen_ty_2 { addr: ptr as _ };
+        submission.0.len = len;
+        /* TODO(port): add back support for read buffer pool.
+        if let Some(buf_group) = buf.buffer_group() {
+            submission.set_buffer_select(buf_group.0);
+        }
+        */
+    }
+
+    /// Check the result of an operation based on the `QueuedOperation.state`
+    /// (`Self::CompletionState`).
+    fn check_result<D: Descriptor>(
+        fd: &AsyncFd<D>,
+        buf: &mut Self::Resources,
+        offset: &mut Self::Args,
+        state: &mut Self::CompletionState,
+    ) -> OpResult<Self::OperationOutput> {
+        state.as_op_result()
+    }
+
+    fn map_ok(mut buf: Self::Resources, (buf_idx, n): Self::OperationOutput) -> Self::Output {
+        // SAFETY: kernel just initialised the bytes for us.
+        unsafe {
+            /* TODO(port): add back support for read buffer pool.
+            buf.buffer_init(BufIdx(buf_idx), n as u32)
+            */
+            buf.set_init(n as usize)
+        };
+        buf
+    }
+}
