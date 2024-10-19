69ca04b23b1c3c8f7dcfa73c67e5199b13a3e8c5
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-10-19 15:40:45 +0200

    Add manual links to kqueue module

--- src/kqueue/mod.rs
+++ src/kqueue/mod.rs
@@ -1,4 +1,10 @@
 //! kqueue implementation.
+//!
+//! Manuals:
+//! * <https://man.freebsd.org/cgi/man.cgi?query=kqueue>
+//! * <https://man.openbsd.org/kqueue>
+//! * <https://www.dragonflybsd.org/cgi/web-man/?command=kqueue>
+//! * <https://man.netbsd.org/kqueue.2>
 
 use std::os::fd::{AsRawFd, OwnedFd};
 use std::sync::Mutex;
