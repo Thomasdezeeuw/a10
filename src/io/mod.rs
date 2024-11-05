c9017ab05649795a0b1e29b5a88750eb07fb6f2c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-11-05 14:12:50 +0700

    Remove allow(unused_imports)

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -21,6 +21,7 @@ use crate::{man_link, sys};
 mod traits;
 
 pub use traits::{Buf, BufMut, BufMutSlice, BufSlice};
+#[allow(unused_imports)] // Not used by all OS.
 pub(crate) use traits::{BufGroupId, BufId};
 // Re-export so we don't have to worry about import `std::io` and `crate::io`.
 pub(crate) use std::io::*;
