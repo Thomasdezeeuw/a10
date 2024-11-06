570d5760ccb4c4173919872fa73fda89d38ad473
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-11-06 11:12:26 +0700

    Add back Std{in,out,err}

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -33,6 +33,58 @@ pub(crate) use std::io::*;
 /// `-1` cast as `unsigned long long` in C is the same as as `u64::MAX`.
 pub(crate) const NO_OFFSET: u64 = u64::MAX;
 
+/// Create a function and type to wraps standard {in,out,error}.
+macro_rules! stdio {
+    (
+        $fn: ident () -> $name: ident, $fd: expr
+    ) => {
+        #[doc = concat!("Create a new [`", stringify!($name), "`].")]
+        pub fn $fn(sq: $crate::SubmissionQueue) -> $name {
+            unsafe { $name(::std::mem::ManuallyDrop::new($crate::fd::AsyncFd::from_raw_fd($fd, sq))) }
+        }
+
+        #[doc = concat!(
+            "An [`AsyncFd`] for ", stringify!($fn), ".\n\n",
+            "Created by calling [`", stringify!($fn), "`].\n\n",
+            "# Notes\n\n",
+            "This directly writes to the raw file descriptor, which means it's not buffered and will not flush anything buffered by the standard library.\n\n",
+            "When this type is dropped it will not close ", stringify!($fn), ".",
+        )]
+        pub struct $name(::std::mem::ManuallyDrop<$crate::fd::AsyncFd>);
+
+        impl ::std::ops::Deref for $name {
+            type Target = $crate::fd::AsyncFd;
+
+            fn deref(&self) -> &Self::Target {
+                &self.0
+            }
+        }
+
+        impl ::std::fmt::Debug for $name {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_struct(::std::stringify!($name))
+                    .field("fd", &*self.0)
+                    .finish()
+            }
+        }
+
+        impl ::std::ops::Drop for $name {
+            fn drop(&mut self) {
+                // We don't want to close the file descriptor, but we do need to
+                // drop our reference to the submission queue.
+                // SAFETY: with `ManuallyDrop` we don't drop the `AsyncFd` so
+                // it's not dropped twice. Otherwise we get access to it using
+                // safe methods.
+                unsafe { ::std::ptr::drop_in_place(&mut self.0.sq) };
+            }
+        }
+    };
+}
+
+stdio!(stdin() -> Stdin, libc::STDIN_FILENO);
+stdio!(stdout() -> Stdout, libc::STDOUT_FILENO);
+stdio!(stderr() -> Stderr, libc::STDERR_FILENO);
+
 /// I/O system calls.
 impl<D: Descriptor> AsyncFd<D> {
     /// Read from this fd into `buf`.
