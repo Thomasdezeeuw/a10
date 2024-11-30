5a77e57e46c40e4632b735f8c76ded9db75a8139
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-11-30 15:57:36 +0100

    Make is_send_and_sync for Direct Linux only

--- tests/async_fd.rs
+++ tests/async_fd.rs
@@ -2,7 +2,7 @@
 
 #![cfg_attr(feature = "nightly", feature(async_iterator))]
 
-use a10::fd::{AsyncFd, Direct, File};
+use a10::fd::{AsyncFd, File};
 
 mod util;
 use util::{is_send, is_sync};
@@ -16,9 +16,15 @@ mod async_fd {
 }
 
 #[test]
-fn is_send_and_sync() {
+fn async_fd_is_send_and_sync() {
     is_send::<AsyncFd<File>>();
     is_sync::<AsyncFd<File>>();
+}
+
+#[test]
+#[cfg(any(target_os = "linux"))]
+fn async_direct_fd_is_send_and_sync() {
+    use a10::fd::Direct;
     is_send::<AsyncFd<Direct>>();
     is_sync::<AsyncFd<Direct>>();
 }
