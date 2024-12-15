2ec6a52388cceae1606bb8220224dd3ab0f546c5
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-12-15 21:35:27 +0100

    Add io_uring and kqueue Op traits

--- src/io_uring/mod.rs
+++ src/io_uring/mod.rs
@@ -244,6 +244,56 @@ impl Drop for Shared {
 }
 
 /// io_uring specific [`crate::op::Op`] trait.
+pub(crate) trait Op {
+    type Output;
+    type Resources;
+    type Args;
+
+    fn fill_submission(
+        resources: &mut Self::Resources,
+        args: &mut Self::Args,
+        submission: &mut sq::Submission,
+    );
+
+    fn map_ok(resources: Self::Resources, op_output: cq::OpReturn) -> Self::Output;
+}
+
+impl<T: Op> crate::op::Op for T {
+    type Output = T::Output;
+    type Resources = T::Resources;
+    type Args = T::Args;
+    type Submission = sq::Submission;
+    type OperationState = cq::OperationState;
+    type OperationOutput = cq::OpReturn;
+
+    fn fill_submission(
+        resources: &mut Self::Resources,
+        args: &mut Self::Args,
+        submission: &mut Self::Submission,
+    ) {
+        T::fill_submission(resources, args, submission)
+    }
+
+    fn check_result(
+        _: &mut Self::Resources,
+        _: &mut Self::Args,
+        state: &mut Self::OperationState,
+    ) -> OpResult<Self::OperationOutput> {
+        match state {
+            cq::OperationState::Single { result } => result.as_op_result(),
+            cq::OperationState::Multishot { results } if results.is_empty() => {
+                OpResult::Again(false)
+            }
+            cq::OperationState::Multishot { results } => results.remove(0).as_op_result(),
+        }
+    }
+
+    fn map_ok(resources: Self::Resources, op_output: Self::OperationOutput) -> Self::Output {
+        T::map_ok(resources, op_output)
+    }
+}
+
+/// io_uring specific [`crate::op::FdOp`] trait.
 pub(crate) trait FdOp {
     type Output;
     type Resources;
