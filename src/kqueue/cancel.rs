ccb5a85f5320f0b394ff28fd77f6e31b015deee6
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-12-15 20:40:16 +0100

    Rename sys::CancelAll to CancelAllOp
    To avoid the name conflict with the Future type.

--- src/kqueue/cancel.rs
+++ src/kqueue/cancel.rs
@@ -8,16 +8,16 @@ pub(crate) fn operation(_: OperationId, _: &mut sys::Event) {
     unimplemented!("kqueue: cancel::operation")
 }
 
-pub(crate) struct CancelAll;
+pub(crate) struct CancelAllOp;
 
-impl sys::Op for CancelAll {
+impl sys::Op for CancelAllOp {
     type Output = usize;
     type Resources = ();
     type Args = ();
     type OperationOutput = usize;
 
     fn fill_submission<D: Descriptor>(_: &AsyncFd<D>, _: &mut sys::Event) {
-        unimplemented!("kqueue: CancelAll::fill_submission")
+        unimplemented!("kqueue: CancelAllOp::fill_submission")
     }
 
     fn check_result<D: Descriptor>(
@@ -25,10 +25,10 @@ impl sys::Op for CancelAll {
         _: &mut Self::Resources,
         _: &mut Self::Args,
     ) -> OpResult<Self::OperationOutput> {
-        unimplemented!("kqueue: CancelAll::check_result")
+        unimplemented!("kqueue: CancelAllOp::check_result")
     }
 
     fn map_ok(_: Self::Resources, _: Self::OperationOutput) -> Self::Output {
-        unimplemented!("kqueue: CancelAll::map_ok")
+        unimplemented!("kqueue: CancelAllOp::map_ok")
     }
 }
