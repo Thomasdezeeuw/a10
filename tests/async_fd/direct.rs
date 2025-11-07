b04c94eb8199634fae8a6b77d63bdf915a3b152c
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-11-08 00:57:34 +0100

    Rewrite changing descriptor tests
    Move more logic into the shared function.

--- tests/async_fd/direct.rs
+++ tests/async_fd/direct.rs
@@ -1,44 +1,44 @@
 //! Tests for the usage of direct descriptors.
 
-use std::sync::Arc;
-
+use a10::fd;
 use a10::fs::OpenOptions;
-use a10::{fd, AsyncFd};
 
 use crate::util::{require_kernel, test_queue, Waker, LOREM_IPSUM_5};
 
 #[test]
 fn to_direct_descriptor() {
-    let sq = test_queue();
-    let waker = Waker::new();
-
-    let open_file = OpenOptions::new().open(sq, LOREM_IPSUM_5.path.into());
-    let regular_fd = waker.block_on(open_file).unwrap();
-    let direct_fd = waker.block_on(regular_fd.to_direct_descriptor()).unwrap();
-
-    check_fs_fd(waker, regular_fd, direct_fd);
+    require_kernel!(5, 5);
+    test_change_descriptor_type(fd::Kind::File);
 }
 
 #[test]
 fn to_file_descriptor() {
     require_kernel!(6, 8);
+    test_change_descriptor_type(fd::Kind::Direct);
+}
 
+fn test_change_descriptor_type(fd_kind: fd::Kind) {
     let sq = test_queue();
     let waker = Waker::new();
 
     let open_file = OpenOptions::new()
-        .kind(fd::Kind::Direct)
+        .kind(fd_kind)
         .open(sq, LOREM_IPSUM_5.path.into());
-    let direct_fd = waker.block_on(open_file).unwrap();
-    let regular_fd = waker.block_on(direct_fd.to_file_descriptor()).unwrap();
+    let fd = waker.block_on(open_file).unwrap();
 
-    check_fs_fd(waker, regular_fd, direct_fd);
-}
+    let (regular_fd, direct_fd) = if let fd::Kind::File = fd_kind {
+        let regular_fd = fd;
+        let direct_fd = waker.block_on(regular_fd.to_direct_descriptor()).unwrap();
+        (regular_fd, direct_fd)
+    } else {
+        let direct_fd = fd;
+        let regular_fd = waker.block_on(direct_fd.to_file_descriptor()).unwrap();
+        (regular_fd, direct_fd)
+    };
 
-fn check_fs_fd(waker: Arc<Waker>, regular_fd: AsyncFd, direct_fd: AsyncFd) {
     let mut buf = Vec::with_capacity(LOREM_IPSUM_5.content.len() + 1);
 
-    // Regular fd.
+    // Regular file descriptor.
     let read = regular_fd.read_at(buf, 0);
     buf = waker.block_on(read).unwrap();
     if buf.is_empty() {
@@ -82,8 +82,6 @@ fn direct_to_direct_descriptor() {
 #[test]
 #[should_panic = "can't covert a file descriptor to a different file descriptor"]
 fn file_to_file_descriptor() {
-    require_kernel!(6, 8);
-
     let sq = test_queue();
     let waker = Waker::new();
 
