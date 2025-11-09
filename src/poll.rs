a5ee02355d96f351b93ecb1ac439db4c87f76e68
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-11-09 23:45:40 +0100

    Use epoll flags in PollEvent
    This requires IORING_FEAT_POLL_32BITS, available since kernel 5.9, so it
    should be safe to use. Furthermore the lower values that are also in
    poll have the same values as in epoll.

--- src/poll.rs
+++ src/poll.rs
@@ -75,68 +75,67 @@ pub struct PollEvent(pub(crate) libc::c_int);
 impl PollEvent {
     /// There is data to read.
     #[doc(alias = "POLLIN")]
+    #[doc(alias = "EPOLLIN")]
     pub const fn is_readable(&self) -> bool {
-        (self.0 & libc::POLLIN as libc::c_int) != 0
+        (self.0 & libc::EPOLLIN) != 0
     }
 
     /// There is some exceptional condition on the file descriptor.
     #[doc(alias = "POLLPRI")]
+    #[doc(alias = "EPOLLPRI")]
     pub const fn is_priority(&self) -> bool {
-        (self.0 & libc::POLLPRI as libc::c_int) != 0
+        (self.0 & libc::EPOLLPRI) != 0
     }
 
     /// Writing is now possible.
     #[doc(alias = "POLLOUT")]
+    #[doc(alias = "EPOLLOUT")]
     pub const fn is_writable(&self) -> bool {
-        (self.0 & libc::POLLOUT as libc::c_int) != 0
+        (self.0 & libc::EPOLLOUT) != 0
+    }
+
+    /// Error condition.
+    #[doc(alias = "POLLERR")]
+    #[doc(alias = "EPOLLERR")]
+    pub const fn is_error(&self) -> bool {
+        (self.0 & libc::EPOLLERR) != 0
+    }
+
+    /// Hang up.
+    #[doc(alias = "POLLHUP")]
+    #[doc(alias = "EPOLLHUP")]
+    pub const fn is_hup(&self) -> bool {
+        (self.0 & libc::EPOLLHUP) != 0
     }
 
     /// Stream socket peer closed connection, or shut down writing half of
     /// connection.
     #[doc(alias = "POLLRDHUP")]
+    #[doc(alias = "EPOLLRDHUP")]
     pub const fn is_read_hup(&self) -> bool {
-        (self.0 & libc::POLLRDHUP as libc::c_int) != 0
-    }
-
-    /// Error condition.
-    #[doc(alias = "POLLERR")]
-    pub const fn is_error(&self) -> bool {
-        (self.0 & libc::POLLERR as libc::c_int) != 0
-    }
-
-    /// Hang up.
-    #[doc(alias = "POLLHUP")]
-    pub const fn is_hup(&self) -> bool {
-        (self.0 & libc::POLLHUP as libc::c_int) != 0
-    }
-
-    /// Returns a bitmask indicating which events occured, see the `poll(2)`
-    /// system call manual and `libc::POLL*` constants, e.g. `libc::POLLIN`.
-    pub const fn events_mask(&self) -> libc::c_int {
-        self.0
+        (self.0 & libc::EPOLLRDHUP) != 0
     }
 }
 
-/// Known poll events supported by Linux as of v6.3.
-const KNOWN_EVENTS: [(libc::c_short, &str); 11] = [
-    (libc::POLLIN, "POLLIN"),
-    (libc::POLLPRI, "POLLPRI"),
-    (libc::POLLOUT, "POLLOUT"),
-    (libc::POLLERR, "POLLERR"),
-    (libc::POLLHUP, "POLLHUP"),
-    (libc::POLLNVAL, "POLLNVAL"),
-    (libc::POLLRDNORM, "POLLRDNORM"),
-    (libc::POLLRDBAND, "POLLRDBAND"),
-    (libc::POLLWRNORM, "POLLWRNORM"),
-    (libc::POLLWRBAND, "POLLWRBAND"),
-    (libc::POLLRDHUP, "POLLRDHUP"),
+/// Known epoll events supported by Linux as of v6.17.
+const KNOWN_EVENTS: [(libc::c_int, &str); 10] = [
+    (libc::EPOLLIN, "EPOLLIN"),
+    (libc::EPOLLPRI, "EPOLLPRI"),
+    (libc::EPOLLOUT, "EPOLLOUT"),
+    (libc::EPOLLERR, "EPOLLERR"),
+    (libc::EPOLLHUP, "EPOLLHUP"),
+    (libc::EPOLLRDNORM, "EPOLLRDNORM"),
+    (libc::EPOLLRDBAND, "EPOLLRDBAND"),
+    (libc::EPOLLWRNORM, "EPOLLWRNORM"),
+    (libc::EPOLLWRBAND, "EPOLLWRBAND"),
+    (libc::EPOLLRDHUP, "EPOLLRDHUP"),
 ];
 
 impl fmt::Debug for PollEvent {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let events = KNOWN_EVENTS
             .into_iter()
-            .filter_map(|(event, name)| (self.0 & libc::c_int::from(event) != 0).then_some(name));
+            .filter_map(|(event, name)| (self.0 & event != 0).then_some(name));
         f.debug_list().entries(events).finish()
     }
 }
