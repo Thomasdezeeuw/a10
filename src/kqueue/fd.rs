4042440d77f32a0c2be14dc60f21be078719abef
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2025-12-26 14:33:55 +0100

    Make Kind::Direct Linux only

--- src/kqueue/fd.rs
+++
@@ -1,9 +0,0 @@
-use crate::kqueue;
-
-pub(crate) fn use_direct_flags(submission: &mut kqueue::Event) {
-    unreachable!("use_direct_flags: kqueue doesn't have direct descriptors")
-}
-
-pub(crate) fn create_direct_flags(submission: &mut kqueue::Event) {
-    unreachable!("create_direct_flags: kqueue doesn't have direct descriptors")
-}
