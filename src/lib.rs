e4a10cd7b87617ad3f734f2b31843ab56876e450
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-01-03 16:31:09 +0100

    Add back syscall! macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,19 @@
 mod io_uring;
 #[cfg(any(target_os = "android", target_os = "linux"))]
 use io_uring as sys;
+
+/// Helper macro to execute a system call that returns an `io::Result`.
+macro_rules! syscall {
+    ($fn: ident ( $($arg: expr),* $(,)? ) ) => {{
+        #[allow(unused_unsafe)]
+        let res = unsafe { ::libc::$fn($( $arg, )*) };
+        if res == -1 {
+            ::std::result::Result::Err(::std::io::Error::last_os_error())
+        } else {
+            ::std::result::Result::Ok(res)
+        }
+    }};
+}
+
+#[allow(unused_imports)] // Not used on all OS.
+use syscall;
