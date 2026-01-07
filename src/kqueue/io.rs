8978d31328e639cba011ef58345f77917cc8cbe1
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-01-07 10:00:05 +0100

    Implement kqueue::io::ReadOp

--- src/kqueue/io.rs
+++ src/kqueue/io.rs
@@ -1,4 +1,44 @@
+use std::io;
+use std::marker::PhantomData;
+
+use crate::io::{Buf, BufId, BufMut, BufMutSlice, BufSlice, NO_OFFSET};
+use crate::kqueue::fd::OpKind;
+use crate::kqueue::{self, cq, sq, Event};
+use crate::{asan, fd, msan, syscall, AsyncFd, SubmissionQueue};
+
 // Re-export so we don't have to worry about import `std::io` and `crate::io`.
 pub(crate) use std::io::*;
 
 pub(crate) use crate::unix::{IoMutSlice, IoSlice};
+
+pub(crate) struct ReadOp<B>(PhantomData<*const B>);
+
+impl<B: BufMut> kqueue::op::FdOp for ReadOp<B> {
+    type Output = B;
+    type Resources = B;
+    type Args = u64; // Offset.
+    type OperationOutput = libc::ssize_t;
+
+    const OP_KIND: OpKind = OpKind::Read;
+
+    fn try_run(
+        fd: &AsyncFd,
+        buf: &mut Self::Resources,
+        offset: &mut Self::Args,
+    ) -> io::Result<Self::OperationOutput> {
+        let (ptr, len) = unsafe { buf.parts_mut() };
+        // io_uring uses `NO_OFFSET` to issue a `read` system call, otherwise it
+        // uses `pread`. We emulate the same thing.
+        if *offset == NO_OFFSET {
+            syscall!(read(fd.fd(), ptr.cast(), len as _))
+        } else {
+            syscall!(pread(fd.fd(), ptr.cast(), len as _, *offset as _))
+        }
+    }
+
+    fn map_ok(fd: &AsyncFd, mut buf: Self::Resources, n: Self::OperationOutput) -> Self::Output {
+        // SAFETY: kernel just initialised the bytes for us.
+        unsafe { buf.set_init(n as _) };
+        buf
+    }
+}
