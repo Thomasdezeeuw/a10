de37ab037a4e17020cfd30903c8d89787fc51970
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-01-12 23:48:57 +0100

    Implement OpenOp for kqueue

--- src/kqueue/fs.rs
+++ src/kqueue/fs.rs
@@ -1,6 +1,45 @@
+use std::ffi::CString;
+use std::io;
+use std::path::PathBuf;
 use std::time::{Duration, SystemTime};
 
-use crate::fs::{FileType, Permissions};
+use crate::fs::{path_from_cstring, FileType, Permissions};
+use crate::kqueue::op::{DirectOp, DirectOpEtract};
+use crate::{fd, syscall, AsyncFd, SubmissionQueue};
+
+pub(crate) struct OpenOp;
+
+impl DirectOp for OpenOp {
+    type Output = AsyncFd;
+    type Resources = (CString, fd::Kind); // path.
+    type Args = (libc::c_int, libc::mode_t); // flags, mode.
+
+    fn run(
+        sq: &SubmissionQueue,
+        resources: Self::Resources,
+        args: Self::Args,
+    ) -> io::Result<Self::Output> {
+        Ok(Self::run_extract(sq, resources, args)?.0)
+    }
+}
+
+impl DirectOpEtract for OpenOp {
+    type ExtractOutput = (AsyncFd, PathBuf);
+
+    fn run_extract(
+        sq: &SubmissionQueue,
+        (path, kind): Self::Resources,
+        (mut flags, mode): Self::Args,
+    ) -> io::Result<Self::ExtractOutput> {
+        let fd::Kind::File = kind;
+
+        flags |= libc::O_NONBLOCK; // NOTE: O_CLOEXEC is already set.
+        let fd = syscall!(open(path.as_ptr(), flags, mode as libc::c_int))?;
+        // SAFETY: just created the fd above.
+        let fd = unsafe { AsyncFd::from_raw(fd, kind, sq.clone()) };
+        Ok((fd, path_from_cstring(path)))
+    }
+}
 
 pub(crate) use libc::stat as Stat;
 
