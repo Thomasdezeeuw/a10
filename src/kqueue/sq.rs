5292fe931d19c1cac9bdc18ec99281ed7a3b2060
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-01-18 00:17:57 +0100

    DRY calling of kevent
    Single function in which we submit changes and process readiness events.

--- src/kqueue/sq.rs
+++ src/kqueue/sq.rs
@@ -1,11 +1,10 @@
 use std::io;
 use std::mem::{self, drop as unlock, take};
-use std::os::fd::AsRawFd;
 use std::sync::Arc;
 use std::sync::atomic::Ordering;
 
 use crate::kqueue::{Event, Shared, cq};
-use crate::{lock, syscall};
+use crate::lock;
 
 #[derive(Clone, Debug)]
 pub(crate) struct Submissions {
@@ -55,43 +54,13 @@ impl Submissions {
         unlock(change_list); // Unlock, to not block others.
 
         // Submit the all changes to the kernel.
-        let timeout = libc::timespec {
+        let ts = libc::timespec {
             tv_sec: 0,
             tv_nsec: 0,
         };
         log::trace!(changes = changes.len(); "submitting changes");
-        let result = syscall!(kevent(
-            shared.kq.as_raw_fd(),
-            // SAFETY: casting `Event` to `libc::kevent` is safe due to
-            // `repr(transparent)` on `Event`.
-            changes.as_ptr().cast(),
-            changes.len().cast_signed() as _,
-            // SAFETY: Same cast as above.
-            changes.as_mut_ptr().cast(),
-            changes.capacity().cast_signed() as _,
-            &raw const timeout,
-        ));
-        if let Err(err) = result {
-            // According to the manual page of FreeBSD: "When kevent() call
-            // fails with EINTR error, all changes in the changelist have been
-            // applied", so we can safely ignore it.
-            if err.raw_os_error() != Some(libc::EINTR) {
-                log::warn!(change_list:? = changes; "failed to submit change list: {err}, dropping changes");
-            }
-        }
-        // Check all events for possible errors and log them.
-        for event in &changes {
-            // NOTE: this can happen if one of the file descriptors was closed
-            // before the change was submitted to the kernel. We'll log it, but
-            // otherwise ignore it.
-            if let Some(err) = event.error() {
-                log::warn!(kevent:? = event; "submitted change has an error: {err}, dropping it");
-            }
-        }
-
-        // Reuse the change list allocation (if it makes sense).
-        changes.clear();
-        shared.merge_change_list(changes);
+        shared.kevent(&mut changes, None, Some(&ts));
+        shared.reuse_change_list(changes);
     }
 
     #[allow(clippy::unnecessary_wraps)]
